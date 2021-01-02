abde0945dcf483073d012bd2eb2f1bb752b69b2a
Sparshagarwal12 <mangalsanchit20@gmail.com>
2021-01-02 21:38:38 +0530

    All Done

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask,render_template,redirect,url_for,request,session,flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "Hello"
 app.permanent_session_lifetime = timedelta(days=5)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+db = SQLAlchemy(app)
+
+
+class users(db.Model):
+    _id = db.Column("id",db.Integer,primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self,name,email):
+        self.name = name
+        self.email = email
 
 @app.route("/")
 def home():
-
     return render_template("index.html")
+
+@app.route("/views")
+def views():
+    return render_template("views.html",values = users.query.all())
 
 @app.route("/login",methods = ["POST","GET"])
 def login():
@@ -16,6 +33,14 @@ def login():
         user = request.form["nm"]
         session.permanent = True
         session["user"] = user
+        found_user = users.query.filter_by(name = user).first()
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = users(user,"")
+            db.session.add(usr)
+            db.session.commit()
+
         flash(f"{user} logged in successfully")
         return redirect(url_for('user'))
     else:
@@ -24,18 +49,30 @@ def login():
             return redirect(url_for("user"))
         return render_template("login.html")
 
-@app.route("/user")
+@app.route("/user",methods = ["POST","GET"])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
-        return render_template("user.html",user = user)
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            found_user = users.query.filter_by(name = user).first()
+            found_user.email = email
+            db.session.commit()
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html",email = email)
     else:
         return "<h1>No User</h1>"
 
 @app.route("/logout")
 def logout():
     session.pop("user",None)
+    session.pop("email",None)
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
